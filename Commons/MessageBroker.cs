63a4d8c833476bd9048f943d9b7cc1dd6e6faf9e
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-05-22 23:00:58 +0900

    MessageBrokerクラス：購読機能 #2

--- Commons/MessageBroker.cs
+++ Commons/MessageBroker.cs
@@ -2,4 +2,23 @@ namespace SubPub_Practice.Commons;
 
 public static class MessageBroker
 {
+    /// <summary>
+    /// キーワード別アクションリスト
+    /// </summary>
+    private static Dictionary<string, List<Action<object>>> Subscribers = [];
+
+    /// <summary>
+    /// 購買メソッド
+    /// </summary>
+    /// <param name="keyword">キーワード</param>
+    /// <param name="action">発行時に呼ばれるメソッド</param>
+    public static void Subscribe(string keyword, Action<object> action)
+    {
+        // キーワードが存在しない場合は生成
+        if (!Subscribers.ContainsKey(keyword))
+            Subscribers.Add(keyword, []);
+
+        // 発行時に呼ばれるメソッドを追加
+        Subscribers[keyword].Add(action);
+    }
 }
