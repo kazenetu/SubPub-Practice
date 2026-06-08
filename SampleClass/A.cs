c8b73eff75351fc9954f304ea17f6edc7fdd2144
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-06-09 00:00:12 +0900

    Aクラスを追加 #3

---
+++ SampleClass/A.cs
@@ -0,0 +1,13 @@
+using SubPub_Practice.Commons;
+
+namespace SubPub_Practice.SampleClass;
+
+public class A
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public A()
+    {
+    }
+}
