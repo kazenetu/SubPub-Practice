58ceb617de90d03cd4570c22a96222a80ce42445
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-06-09 22:12:47 +0900

    Aクラス：発行メソッドを追加 #3

--- SampleClass/A.cs
+++ SampleClass/A.cs
@@ -21,6 +21,14 @@ public class A : IDisposable
     }
 
     /// <summary>
+    /// 発行
+    /// </summary>
+    public void Publish()
+    {
+        MessageBroker.Publish("test", 1m);
+    }
+
+    /// <summary>
     /// MessageBroker発行時のコールバック
     /// </summary>
     /// <param name="data">発行時に送信された情報</param>
