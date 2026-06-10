249f029f34b958715d2ab952e560fb61fa5c9554
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-06-11 00:28:23 +0900

    Bクラス：購読用メソッドを追加 #3

--- SampleClass/B.cs
+++ SampleClass/B.cs
@@ -8,4 +8,13 @@ public class B
     public B()
     {
     }
+
+    /// <summary>
+    /// MessageBroker発行時のコールバック
+    /// </summary>
+    /// <param name="data">発行時に送信された情報</param>
+    private void Callback(object data)
+    {
+        Console.WriteLine($"CallBack A! deta is {data}");
+    }
 }
