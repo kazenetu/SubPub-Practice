1d5522d7223753834634cf923f82bb7e0ae3ea94
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-06-13 23:41:48 +0900

    Cクラス：購読用メソッドを追加 #3

--- SampleClass/C.cs
+++ SampleClass/C.cs
@@ -1,5 +1,22 @@
+using SubPub_Practice.Commons;
+
 namespace SubPub_Practice.SampleClass;
 
 public class C
 {
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public C()
+    {
+    }
+
+    /// <summary>
+    /// MessageBroker発行時のコールバック
+    /// </summary>
+    /// <param name="data">発行時に送信された情報</param>
+    private void Callback(object data)
+    {
+        Console.WriteLine($"CallBack C! deta is {data}");
+    }
 }
