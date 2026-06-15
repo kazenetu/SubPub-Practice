c3d92f17c5c3922f85ed37506f6218da9b8a8b45
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-06-15 23:17:30 +0900

    Cクラス：Disposeメソッドに購買解除処理を追加 #3

--- SampleClass/C.cs
+++ SampleClass/C.cs
@@ -2,7 +2,7 @@ using SubPub_Practice.Commons;
 
 namespace SubPub_Practice.SampleClass;
 
-public class C
+public class C : IDisposable
 {
     /// <summary>
     /// コンストラクタ
@@ -10,6 +10,14 @@ public class C
     public C()
     {
         MessageBroker.Subscribe("test", Callback);
+    }
+
+    /// <summary>
+    /// 破棄
+    /// </summary>
+    public void Dispose()
+    {
+        MessageBroker.UnSubscribe("test", Callback);
     }
 
     /// <summary>
