587e8c622df3573340b9dfa9485566e721840693
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-06-16 23:01:21 +0900

    Cクラス：発行メソッドを追加 #3

--- SampleClass/C.cs
+++ SampleClass/C.cs
@@ -21,6 +21,14 @@ public class C : IDisposable
     }
 
     /// <summary>
+    /// 発行
+    /// </summary>
+    public void Publish()
+    {
+        MessageBroker.Publish("test", "パラメータ");
+    }
+
+    /// <summary>
     /// MessageBroker発行時のコールバック
     /// </summary>
     /// <param name="data">発行時に送信された情報</param>
