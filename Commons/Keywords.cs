be8f30e6deb8760505f2dc030cb7ad56cd0abc89
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-06-28 22:47:56 +0900

    Keywordsクラス追加 #3

---
+++ Commons/Keywords.cs
@@ -0,0 +1,12 @@
+namespace SubPub_Practice.Commons;
+
+/// <summary>
+/// Publish/Subscribeクラス用キーワード
+/// </summary>
+public static class Keywords
+{
+    /// <summary>
+    /// テスト用キーワード
+    /// </summary>
+    public static string Test = "Test";
+}
