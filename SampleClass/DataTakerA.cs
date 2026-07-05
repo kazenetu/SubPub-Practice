d97552c24178b1201a000489604bbf45dfe83449
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-07-06 00:53:01 +0900

    データ提供クラスA：購買メソッドとCallBackメソッド追加 #6

--- SampleClass/DataTakerA.cs
+++ SampleClass/DataTakerA.cs
@@ -1,6 +1,43 @@
+using System.Collections;
+using SubPub_Practice.Commons;
+
 namespace SubPub_Practice.SampleClass;
 
-public class DataTakerA
+public class DataTakerA : IDisposable
 {
-    
+    public DataTakerA()
+    {
+        // リクエストの購読
+        MessageBroker.Subscribe(Keywords.RequestA, Callback);
+    }
+
+    /// <summary>
+    /// 破棄
+    /// </summary>
+    public void Dispose()
+    {
+        // リクエストの購読解除
+        MessageBroker.UnSubscribe(Keywords.RequestA, Callback);
+    }
+
+    /// <summary>
+    /// MessageBroker購読コールバック
+    /// </summary>
+    /// <param name="data">発行時に送信された情報</param>
+    private void Callback(object data)
+    {
+        var result = data;
+        if (data is IList list)
+        {
+            var resultList = new List<string>();
+            foreach (var item in list)
+            {
+                resultList.Add($"{item}");
+            }
+            result = string.Join(",", resultList);
+        }
+
+        // HACK 発行
+        Console.WriteLine($"CallBack DataTakerA! deta is {result}");
+    }
 }
