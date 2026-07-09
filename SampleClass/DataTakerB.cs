92c48858ad55e624ff39473e12b922df6bc89233
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-07-09 21:47:58 +0900

    データ提供クラスB：購買メソッドとCallBackメソッド追加 #6

--- SampleClass/DataTakerB.cs
+++ SampleClass/DataTakerB.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using SubPub_Practice.Commons;
+
 namespace SubPub_Practice.SampleClass;
 
 /// <summary>
@@ -5,5 +8,39 @@ namespace SubPub_Practice.SampleClass;
 /// </summary>
 public class DataTakerB
 {
-    
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public DataTakerB()
+    {
+        // リクエストの購読
+        MessageBroker.Subscribe(Keywords.RequestB, Callback);
+    }
+
+    /// <summary>
+    /// 破棄
+    /// </summary>
+    public void Dispose()
+    {
+        // リクエストの購読解除
+        MessageBroker.UnSubscribe(Keywords.RequestB, Callback);
+    }
+
+    /// <summary>
+    /// MessageBroker購読コールバック
+    /// </summary>
+    /// <param name="data">発行時に送信された情報</param>
+    private void Callback(object data)
+    {
+        var result = data;
+        if (data is IList list)
+        {
+            var resultList = new List<string>();
+            foreach (var item in list)
+            {
+                resultList.Add($"{item}");
+            }
+            result = string.Join(",", resultList);
+        }
+    }
 }
