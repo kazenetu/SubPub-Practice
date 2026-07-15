6696b1cc20280f8f1aee088d513ba23ae636f658
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-07-16 00:04:36 +0900

    データ取得クラス：プロパティ追加 #6

--- SampleClass/DataGever.cs
+++ SampleClass/DataGever.cs
@@ -5,4 +5,17 @@ namespace SubPub_Practice.SampleClass;
 /// </summary>
 public class DataGever
 {
+    #region プロパティ
+
+    /// <summary>
+    /// データ提供クラスAの提供情報
+    /// </summary>
+    public string? ResultA { private set; get; }
+
+    /// <summary>
+    /// データ提供クラスBの提供情報
+    /// </summary>
+    public string? ResultB { private set; get; }
+
+    #endregion
 }
