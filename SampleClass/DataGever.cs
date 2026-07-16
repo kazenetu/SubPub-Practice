321d915e7bc950c23491e0cd3817015fe0080dfa
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-07-16 22:39:32 +0900

    データ取得クラス：CallBackメソッド追加 #6

--- SampleClass/DataGever.cs
+++ SampleClass/DataGever.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace SubPub_Practice.SampleClass;
 
 /// <summary>
@@ -16,6 +18,52 @@ public class DataGever
     /// データ提供クラスBの提供情報
     /// </summary>
     public string? ResultB { private set; get; }
+
+    #endregion
+
+    #region イベント
+
+    /// <summary>
+    /// MessageBroker購読コールバックA
+    /// </summary>
+    /// <param name="data">発行時に送信された情報</param>
+    private void CallbackResponseA(object data)
+    {
+        var result = data;
+        if (data is IList list)
+        {
+            var resultList = new List<string>();
+            foreach (var item in list)
+            {
+                resultList.Add($"{item}");
+            }
+            result = string.Join(",", resultList);
+        }
+
+        // プロパティに格納
+        ResultA = result as string;
+    }
+
+    /// <summary>
+    /// MessageBroker購読コールバックB
+    /// </summary>
+    /// <param name="data">発行時に送信された情報</param>
+    private void CallbackResponseB(object data)
+    {
+        var result = data;
+        if (data is IList list)
+        {
+            var resultList = new List<string>();
+            foreach (var item in list)
+            {
+                resultList.Add($"{item}");
+            }
+            result = string.Join(",", resultList);
+        }
+
+        // プロパティに格納
+        ResultB = result as string;
+    }
 
     #endregion
 }
