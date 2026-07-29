a597360f433f5480c262a8ffd6ad9f390f25a983
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-07-29 21:04:07 +0900

    ResponseBRecord.cs：データ提供クラスB：データ送信用recordを追加 #6

---
+++ SampleClass/Records/ResponseBRecord.cs
@@ -0,0 +1,8 @@
+namespace SubPub_Practice.SampleClass.Records;
+
+/// <summary>
+///  応答レスポンスBレコード
+/// </summary>
+/// <param name="DataString">文字列データ</param>
+/// <param name="DataInt">数値データ</param>
+public record ResponseBRecord(string DataString, int DataInt);
