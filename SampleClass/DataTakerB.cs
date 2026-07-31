0bfc8149df32d1fa6479752cb8bd0a71d955a4bb
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-07-31 22:09:53 +0900

    データ提供クラスB：CallBackメソッド：「ResponseBRecord」発行に変更 #6

--- SampleClass/DataTakerB.cs
+++ SampleClass/DataTakerB.cs
@@ -7,7 +7,7 @@ namespace SubPub_Practice.SampleClass;
 /// <summary>
 /// データ提供クラスB
 /// </summary>
-public class DataTakerB: IDisposable
+public class DataTakerB : IDisposable
 {
     /// <summary>
     /// コンストラクタ
@@ -28,27 +28,16 @@ public class DataTakerB: IDisposable
     }
 
     /// <summary>
-    /// MessageBroker購読コールバック
+    /// 購読コールバック：ResponseBRecord発行
     /// </summary>
     /// <param name="data">発行時に送信された情報</param>
     private void Callback(object data)
     {
-        var result = data;
-        if (data is IList list)
-        {
-            var resultList = new List<string>();
-            foreach (var item in list)
-            {
-                resultList.Add($"{item}");
-            }
-            result = string.Join(",", resultList);
-        }
-
         // RequestRecordでTargetsが自身ではない場合は終了
         if (data is RequestRecord req && req.Target != RequestRecord.Targets.DataTakerB) return;
 
         // 発行
-        var sendData = "DataTakerB";
-        MessageBroker.Publish(Keywords.ResponseB, sendData);
+        var responseData = new ResponseBRecord("ResponseBRecord!",99);
+        MessageBroker.Publish(Keywords.ResponseB, responseData);
     }
 }
