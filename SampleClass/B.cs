5eecddb4c9bce7b602275288f918e52b86ce84e9
kazenetu <13482856+kazenetu@users.noreply.github.com>
2026-08-18 22:22:06 +0900

    B.cs：購読メソッド：パラメータコメント修正 #11

--- SampleClass/B.cs
+++ SampleClass/B.cs
@@ -47,7 +47,7 @@ public class B : IDisposable
     /// <summary>
     /// 購読コールバック
     /// </summary>
-    /// <param name="data">発行時に送信された情報</param>
+    /// <param name="data">受信情報</param>
     private void Callback(object data)
     {
         var result = data;
